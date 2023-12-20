327c8055e92d1b6afba3d8ae29bfb37f41c72be1
lvxtorry <princessviktoria2007@gmail.com>
2023-12-20 12:54:59 +0300

    createdatebase

--- program_7_1.py
+++ program_7_1.py
@@ -1,11 +1,18 @@
 import matplotlib.pyplot as plt
+import sqlite3
+import nvln
+
 f = open('20046 (1).dat', 'r')
 data = []
+counter = 10
 for s in f.readlines():
     temp = s.split()
     #станция, год, месяц, день, час, направление ветра, осадки, температуа, влажность
     data.append([temp[0], temp[5], temp[6], temp[7], temp[10], float(temp[39]), float(temp[47]), float(temp[-31]), float(temp[-15])])
-print(data[0])
+    print(([temp[0], temp[5], temp[6], temp[7], temp[10], float(temp[39]), float(temp[47]), float(temp[-31]), float(temp[-15])]))
+    #db_add.add_sign(counter, temp)
+    counter += 1
+#print(data[0])
 
 x_range = list(map(int, input().split()))
 
