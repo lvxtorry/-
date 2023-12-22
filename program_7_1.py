17aa3486a1d0f04c80e0fb750a4b202e4e205376
lvxtorry <princessviktoria2007@gmail.com>
2023-12-22 09:16:58 +0300

    createdatebase

--- program_7_1.py
+++ program_7_1.py
@@ -1,45 +1,50 @@
 import matplotlib.pyplot as plt
 import sqlite3
 import nvln
+import lalala
 
-f = open('20046 (1).dat', 'r')
-data = []
-counter = 10
-for s in f.readlines():
-    temp = s.split()
-    #станция, год, месяц, день, час, направление ветра, осадки, температуа, влажность
-    data.append([temp[0], temp[5], temp[6], temp[7], temp[10], float(temp[39]), float(temp[47]), float(temp[-31]), float(temp[-15])])
-    print(([temp[0], temp[5], temp[6], temp[7], temp[10], float(temp[39]), float(temp[47]), float(temp[-31]), float(temp[-15])]))
-    #db_add.add_sign(counter, temp)
-    counter += 1
-#print(data[0])
+data = (1985, 12, 31)
+import lalala
+lalala.query(data)
 
-x_range = list(map(int, input().split()))
+# f = open('20046 (1).dat', 'r')
+# data = []
+# counter = 10
+# for s in f.readlines():
+#     temp = s.split()
+#     # станция, год, месяц, день, час, направление ветра, осадки, температуа, влажность
+#     data.append([temp[0], temp[5], temp[6], temp[7], temp[10], float(temp[39]), float(temp[47]), float(temp[-31]), float(temp[-15])])
+#     print(([temp[0], temp[5], temp[6], temp[7], temp[10], float(temp[39]), float(temp[47]), float(temp[-31]), float(temp[-15])]))
+#     nvln.add_sign(counter, temp)
+#     counter += 1
+# print(data[0])
 
-x = range(len(data[x_range[0]:x_range[1]]))
-y = [i[-2] for i in data[x_range[0]:x_range[1]]]
+# x_range = list(map(int, input().split()))
+#
+# x = range(len(data[x_range[0]:x_range[1]]))
+# y = [i[-2] for i in data[x_range[0]:x_range[1]]]
+#
 
-
-plt.figure(figsize=(7, 7))
-plt.subplot(3, 1, 1)
-plt.title('Температура')
-plt.xlabel('Время')
-plt.ylabel('Температура')
-plt.grid()
-plt.plot(x, y, "g")
-plt.show()
-
-plt.subplot(3, 1, 2)
-plt.title('График y = x^2')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid()
-plt.plot(x, y, "orange")
-
-plt.subplot(3, 1, 3)
-plt.title('График y = x^2')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid()
-plt.plot(x, y, "blue")
-#plt.show()
+# plt.figure(figsize=(7, 7))
+# plt.subplot(3, 1, 1)
+# plt.title('Температура')
+# plt.xlabel('Время')
+# plt.ylabel('Температура')
+# plt.grid()
+# plt.plot(x, y, "g")
+# plt.show()
+#
+# plt.subplot(3, 1, 2)
+# plt.title('График y = x^2')
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.grid()
+# plt.plot(x, y, "orange")
+#
+# plt.subplot(3, 1, 3)
+# plt.title('График y = x^2')
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.grid()
+# plt.plot(x, y, "blue")
+# plt.show()
